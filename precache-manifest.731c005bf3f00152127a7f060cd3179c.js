1be87ca0f1e309ee750db1c7c172cbb7efb028c4
Andrey <Andreysk8er@ukr.net>
2020-09-29 22:07:39 +0300

    Updates

---
+++ precache-manifest.731c005bf3f00152127a7f060cd3179c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23f3b1124b1cb0f1c1818fc96257ad9",
+    "url": "/Clone_Netflix/index.html"
+  },
+  {
+    "revision": "efa4eecec12145c6c087",
+    "url": "/Clone_Netflix/static/css/main.dea43094.chunk.css"
+  },
+  {
+    "revision": "18e0e7051381f44387fd",
+    "url": "/Clone_Netflix/static/js/2.ea432b74.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Clone_Netflix/static/js/2.ea432b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa4eecec12145c6c087",
+    "url": "/Clone_Netflix/static/js/main.139671b8.chunk.js"
+  },
+  {
+    "revision": "35ae1ca19d71cf0d4ff5",
+    "url": "/Clone_Netflix/static/js/runtime-main.351af51e.js"
+  }
+]);
